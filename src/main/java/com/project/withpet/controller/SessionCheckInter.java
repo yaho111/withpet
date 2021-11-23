1ecdafdc7dc59a517c129a51aeb500349b8de209
kkatalchae <byeongchae44@gmail.com>
2021-11-23 10:37:51 +0900

    기능 추가 : 인터셉터 구현
    세션을 확인해 로그인되어 있지 않을 시, 로그인 페이지로 이동하도록 구현

---
+++ src/main/java/com/project/withpet/controller/SessionCheckInter.java
@@ -0,0 +1,24 @@
+package com.project.withpet.controller;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionCheckInter extends HandlerInterceptorAdapter {
+	
+	// preHandle(request,response,handler)메소드
+	// 1.Controller에서 요청(*.nhn)을 받기 전에  preHandle()가 호출되어 가로채는 역할로 사용
+	// 2.로그인 하지않고(세션이 없으면) 요청하면 로그인 폼으로 이동 하도록 해주는 역할
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("id");
+		if (id == null || id.equals(""))  {		
+			response.sendRedirect("loginForm");	// 세션이 없으면 로그인 폼으로 이동
+			return false;
+		}
+		return true;
+	}
+}
