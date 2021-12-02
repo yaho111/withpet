f583dd0b95eb795497fbc714d8363489da4957a4
jin.danid <jin.danid@gmaiil.com>
2021-12-02 20:00:14 +0900

    기능추가 : Q/A 댓글
    댓글 유효성 검사 파일

---
+++ src/main/webapp/js/qnaRe.js
@@ -0,0 +1,13 @@
+function qnaRe_check() {
+	if ($.trim($("#qna_title").val()) == "") {
+		alert("제목를 입력하세요!");
+		$("#qna_title").val("").focus();
+		return false;
+	}
+	if(CKEDITOR.instances.qna_content.getData() =='' 
+        || CKEDITOR.instances.qna_content.getData().length ==0){
+    alert("질문을 입력하세요!");
+    $("#qna_content").focus();
+    return false;
+	}
+}
